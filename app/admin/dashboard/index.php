d2b2d3d9fa6bba5db9593b6fad4c6c419be17e38
Ulfatun Nasikhah <ulfatunnasikhah49@gmail.com>
2021-11-10 09:23:11 +0700

    1. Membuat tampilan dashboard 2. Membuat isi dashboard

---
+++ app/admin/dashboard/index.php
@@ -0,0 +1,144 @@
+<?php
+include '../../config/f_pengajuan.php';
+
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pengajuan Cuti dan Izin Aktif</title>
+
+    <?php
+    include "../../../public/rel.php";
+    ?>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <?php
+        include "../../../public/header.php";
+        ?>
+        <!-- /.Navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php include "../../../public/sidebar.php"; ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Dashboard</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item">Dashboard</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <?php
+                                    $sql = mysqli_query($conn, "SELECT count(*) AS jml_pgw FROM tb_pegawai");
+                                    $hasil = mysqli_fetch_array($sql);
+                                    ?>
+                                    <h3><?php echo $hasil['jml_pgw']; ?></h3>
+
+                                    <p>Data Pegawai</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-bag"></i>
+                                </div>
+                                <a href="../pegawai/" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <!-- ./col -->
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-success">
+                                <div class="inner">
+                                    <?php
+                                    $sql = mysqli_query($conn, "SELECT count(*) AS jml_mhs FROM tb_mahasiswa");
+                                    $hasil = mysqli_fetch_array($sql);
+                                    ?>
+                                    <h3><?php echo $hasil['jml_mhs']; ?></h3>
+
+                                    <p>Data Mahasiswa</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-stats-bars"></i>
+                                </div>
+                                <a href="../mahasiswa/" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <!-- ./col -->
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <?php
+                                    $sql = mysqli_query($conn, "SELECT count(*) AS jml_pengajuan FROM tb_pengajuan");
+                                    $hasil = mysqli_fetch_array($sql);
+                                    ?>
+                                    <h3><?php echo $hasil['jml_pengajuan']; ?></h3>
+                                    <p>Data Pengajuan</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-person-add"></i>
+                                </div>
+                                <a href="../pengajuan/" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <!-- ./col -->
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-danger">
+                                <div class="inner">
+                                    <?php
+                                    $sql = mysqli_query($conn, "SELECT count(*) AS jml_pengajuan FROM tb_pengajuan WHERE status < '4'");
+                                    $hasil = mysqli_fetch_array($sql);
+                                    ?>
+                                    <h3><?php echo $hasil['jml_pengajuan']; ?></h3>
+                                    <p>Pengajuan Belum Selesai Diverifikasi</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-pie-graph"></i>
+                                </div>
+                                <a href="../pengajuan/" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <!-- ./col -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+
+        <!-- footer -->
+        <?php include "../../../public/footer.php"; ?>
+        <!-- script -->
+        <?php include "../../../public/script.php"; ?>
+
+    </div>
+</body>
+
+</html>
