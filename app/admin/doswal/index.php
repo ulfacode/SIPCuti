055b461ce26a1ee2934c31783586306262b1bf15
Ulfatun Nasikhah <ulfatunnasikhah49@gmail.com>
2021-12-16 13:29:34 +0700

    nothing

--- app/admin/doswal/index.php
+++ app/admin/doswal/index.php
@@ -172,7 +172,7 @@ session_start();
         </script>
 
         <!--datatable -->
-        <script script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js">
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js">
         </script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
