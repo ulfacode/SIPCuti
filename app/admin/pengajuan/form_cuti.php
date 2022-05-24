76d6c10638e1a8df364603ccdd62736f00403602
Ulfatun Nasikhah <ulfatunnasikhah49@gmail.com>
2022-05-24 15:42:36 +0700

    Memperbaiki query select dan ttd; menambah persetujuan bag keu

--- app/admin/pengajuan/form_cuti.php
+++ app/admin/pengajuan/form_cuti.php
@@ -15,26 +15,66 @@ if ($result['nim']) {
         $kelas = $data['kelas'];
         $alamat = $data['alamat'];
         $no_telp = $data['no_telp'];
-        $ttd = $data['ttd'];
+        $ttd_mhs = $data['ttd'];
     }
 }
 
 // ambil data doswal
-$sql2 = mysqli_query($conn, "select p.nama, p.ttd, p.nip_npak from tb_doswal as d, tb_pegawai as p WHERE d.nip_npak=p.nip_npak AND id_doswal='$id_doswal'");
-while ($data_doswal = mysqli_fetch_array($sql2)) {
-    $nama_doswal = $data_doswal['nama'];
-    $nip_doswal = $data_doswal['nip_npak'];
-    $ttd_doswal = $data_doswal['ttd'];
-}
-// ambil data kajur
-$kajur = mysqli_query($conn, "select p.nama, p.ttd, p.nip_npak, k.nm_jurusan from tb_pegawai as p, tb_kajur as k WHERE p.nip_npak=k.nip_npak AND id_kajur='$id_kajur'");
-while ($data_kajur = mysqli_fetch_array($kajur)) {
-    $nama_kajur = $data_kajur['nama'];
-    $nm_jurusan = $data_kajur['nm_jurusan'];
-    $nip_kajur = $data_kajur['nip_npak'];
-    $ttd_kajur = $data_kajur['ttd'];
+if ($result['status'] > '1') {
+    $sql2 = mysqli_query($conn, "select p.nama, p.ttd, p.nip_npak, v.status from tb_doswal as d, tb_pegawai as p, tb_verifikasi as v WHERE d.nip_npak=p.nip_npak AND p.nip_npak=v.nip_npak AND id_doswal='$id_doswal'");
+    while ($data_doswal = mysqli_fetch_array($sql2)) {
+        $nama_doswal = $data_doswal['nama'];
+        $nip_doswal = $data_doswal['nip_npak'];
+        $ttd_doswal = $data_doswal['ttd'];
+        $status_doswal = $data_doswal['status'];
+    }
+} else {
+    $sql2 = mysqli_query($conn, "select p.nama, p.ttd, p.nip_npak from tb_doswal as d, tb_pegawai as p WHERE d.nip_npak=p.nip_npak AND id_doswal='$id_doswal'");
+    while ($data_doswal = mysqli_fetch_array($sql2)) {
+        $nama_doswal = $data_doswal['nama'];
+        $nip_doswal = $data_doswal['nip_npak'];
+        $ttd_doswal = $data_doswal['ttd'];
+    }
 }
 
+
+// ambil data kajur
+if ($result['status'] > '2') {
+    $kajur = mysqli_query($conn, "select p.nama, p.ttd, p.nip_npak, k.nm_jurusan, v.status from tb_pegawai as p, tb_kajur as k, tb_verifikasi as v WHERE p.nip_npak=k.nip_npak AND p.nip_npak=v.nip_npak AND id_kajur='$id_kajur'");
+    while ($data_kajur = mysqli_fetch_array($kajur)) {
+        $nama_kajur = $data_kajur['nama'];
+        $nm_jurusan = $data_kajur['nm_jurusan'];
+        $nip_kajur = $data_kajur['nip_npak'];
+        $ttd_kajur = $data_kajur['ttd'];
+        $status_kajur = $data_kajur['status'];
+    }
+} else {
+    $kajur = mysqli_query($conn, "select p.nama, p.ttd, p.nip_npak, k.nm_jurusan from tb_pegawai as p, tb_kajur as k WHERE p.nip_npak=k.nip_npak AND id_kajur='$id_kajur'");
+    while ($data_kajur = mysqli_fetch_array($kajur)) {
+        $nama_kajur = $data_kajur['nama'];
+        $nm_jurusan = $data_kajur['nm_jurusan'];
+        $nip_kajur = $data_kajur['nip_npak'];
+        $ttd_kajur = $data_kajur['ttd'];
+    }
+}   
+
+// ambil data koordinator bagian keuangan
+if (is_null($result['status'])) {
+    $sql3 = mysqli_query($conn, "select p.nama, p.ttd, p.nip_npak from tb_pegawai as p WHERE p.jabatan = 'Bagian Keuangan' AND p.status = 'Aktif'");
+    while ($bag_keu = mysqli_fetch_array($sql3)) {
+        $nama_keu = $bag_keu['nama'];
+        $nip_keu = $bag_keu['nip_npak'];
+        $ttd_keu = $bag_keu['ttd'];
+    }
+} else {
+    $sql3 = mysqli_query($conn, "select p.nama, p.ttd, p.nip_npak, v.status from tb_pegawai as p, tb_pengajuan as pj, tb_verifikasi as v WHERE pj.id_pengajuan = v.id_pengajuan AND p.nip_npak=v.nip_npak AND p.jabatan = 'Bagian Keuangan' AND v.id_pengajuan='$id_pengajuan'");
+    while ($bag_keu = mysqli_fetch_array($sql3)) {
+        $nama_keu = $bag_keu['nama'];
+        $nip_keu = $bag_keu['nip_npak'];
+        $ttd_keu = $bag_keu['ttd'];
+        $status_keu = $bag_keu['status'];
+    }
+}
 
 
 error_reporting(0);
@@ -234,8 +274,7 @@ error_reporting(0);
                 <tr class="text2">
                     <td>
                         <font size="3">Ketentuan: <br></font>
-                        <font size="3">1. Jangka waktu cuti akademik adalah satu tahun (dua semester) <br></font>
-                        <font size="3">2. Melakukan daftar ulang sebesar 25% dari biaya kuliah tiap semester</font>
+                        <font size="3">Jangka waktu cuti akademik adalah satu tahun (dua semester) <br></font>
                     </td>
                 </tr>
             </table>
@@ -259,16 +298,19 @@ error_reporting(0);
             </table>
 
             <table width="600">
+                <!-- row 1 -->
                 <tr>
-                    <td width="230"></td>
-                    <td width=""></td>
-                    <td width="230">Cilacap, <?= tgl($result['tgl_pengajuan']); ?></td>
+                    <td width="200"></td>
+                    <td width="200"></td>
+                    <td width="200">Cilacap, <?= tgl($result['tgl_pengajuan']); ?></td>
                 </tr>
+                <!-- row 2 -->
                 <tr>
                     <td colspan="3">
                         <font size="3">Mengetahui</font>
                     </td>
                 </tr>
+                <!-- row 3 -->
                 <tr>
                     <td align="center">
                         <font size="3">Orang Tua/Wali</font>
@@ -278,7 +320,7 @@ error_reporting(0);
                         <font size="3">Pemohon</font>
                     </td>
                 </tr>
-
+                <!-- row 4 (ttd mhs dan ortu) -->
                 <tr>
                     <?php
                     if (empty($result['ttd_ortu'])) { ?>
@@ -296,42 +338,89 @@ error_reporting(0);
                     ?>
                     <td></td>
                     <?php
-                    if (empty($ttd)) { ?>
+                    if (empty($ttd_mhs)) { ?>
                         <td align="center" width="60" height="60"></td>
                     <?php } else { ?>
-                        <td align="center"><img src="../mahasiswa/img/<?= $ttd ?>" width="60" height="60" alt="tanda tangan mahasiswa"></td>
+                        <td align="center"><img src="../mahasiswa/img/<?= $ttd_mhs ?>" width="60" height="60" alt="tanda tangan mahasiswa"></td>
                     <?php
                     }
                     ?>
                 </tr>
-
+                <!-- row 5 -->
                 <tr>
                     <td align="center"><?= $result['nama_ortu']; ?></td>
                     <td></td>
                     <td align="center"><?= $nama ?></td>
                 </tr>
+                <!-- row 6 -->
                 <tr>
                     <td></td>
                     <td align="center">Menyetujui</td>
                     <td></td>
                 </tr>
+                <!-- row 7 -->
                 <tr>
-                    <td align="center">
-                        Ketua Jurusan
-                    </td>
-                    <td></td>
                     <td align="center">Wali Kelas</td>
-                </tr>
-                <tr>
-                    <td align="center">
-                        <?= $nm_jurusan; ?>
-                    </td>
                     <td></td>
-                    <td></td>
+                    <td align="center">Koordinator Bagian Keuangan</td>
                 </tr>
+                <!-- row 8 -->
                 <tr>
                     <?php
-                    if (($result['status']) > '1') {
+                    if (($result['status']) > '1' and $status_doswal == 'Diterima') {
+                    ?>
+                        <td align="center"><img src="../pegawai/img/<?= $ttd_doswal; ?>" width="60" height="60" alt="tanda tangan doswal"></td>
+                    <?php
+                    } else { ?>
+                        <td align="center" width="60" height="60"></td>
+                    <?php
+                    }
+                    ?>
+
+                    <td></td>
+
+                    <?php
+                    if (!(is_null(($result['status']))) and $status_keu == 'Diterima') {
+                    ?>
+                        <td align="center"><img src="../pegawai/img/<?= $ttd_keu; ?>" width="60" height="60" alt="tanda tangan koordinator bagian keuangan"></td>
+                    <?php
+                    } else { ?>
+                        <td align="center" width="60" height="60"></td>
+                    <?php
+                    }
+                    ?>
+                </tr>
+                <tr>
+                    <td align="center"><?= $nama_doswal; ?></td>
+                    <td></td>
+                    <td align="center"><?= $nama_keu; ?></td>
+                </tr>
+                <tr>
+                    <td align="center">NPAK. <?= $nip_doswal; ?></td>
+                    <td></td>
+                    <td align="center">NPAK. <?= $nip_keu; ?></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td align="center">
+                        <?php if ($nm_jurusan == 'Teknik Pengendalian Pencemaran Lingkungan') {
+                            echo "Koodinator Program Studi";
+                        } else {
+                            echo "Ketua Jurusan";
+                        } ?>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td align="center"><?= $nm_jurusan; ?></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <?php
+                    if (($result['status']) > '2' and $status_kajur == 'Diterima') {
                     ?>
                         <td align="center"><img src="../pegawai/img/<?= $ttd_kajur; ?>" width="60" height="60" alt="tanda tangan kajur"></td>
                     <?php
@@ -341,26 +430,15 @@ error_reporting(0);
                     }
                     ?>
                     <td></td>
-                    <?php
-                    if (!(is_null(($result['status'])))) {
-                    ?>
-                        <td align="center"><img src="../pegawai/img/<?= $ttd_doswal; ?>" width="60" height="60" alt="tanda tangan doswal"></td>
-                    <?php
-                    } else { ?>
-                        <td align="center" width="60" height="60"></td>
-                    <?php
-                    }
-                    ?>
                 </tr>
                 <tr>
+                    <td></td>
                     <td align="center"><?= $nama_kajur; ?></td>
                     <td></td>
-                    <td align="center"><?= $nama_doswal ?></td>
                 </tr>
                 <tr>
-                    <td align="center"><?= $nip_kajur; ?></td>
                     <td></td>
-                    <td align="center"><?= $nip_doswal ?></td>
+                    <td align="center">NPAK. <?= $nip_kajur; ?></td>
                 </tr>
             </table>
         </div>
