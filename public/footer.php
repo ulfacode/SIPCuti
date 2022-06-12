c93379f41c91f725b8ae9e01524e43d83ede5dd7
Ulfatun Nasikhah <ulfatunnasikhah49@gmail.com>
2022-06-12 19:48:44 +0700

    Mengganti tulisan di footer

--- public/footer.php
+++ public/footer.php
@@ -3,7 +3,7 @@
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 1.0.0
     </div>
-    <strong>Ulfatun Nasikhah &copy; 2021</strong> All rights reserved.
+    <strong>Politeknik Negeri Cilacap &copy; 2021</strong> All rights reserved.
 </footer>
 
 <!-- Control Sidebar -->
