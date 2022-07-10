9dd26081022449eaee13e6f99798803dd7a3d4a8
Ulfatun Nasikhah <ulfatunnasikhah49@gmail.com>
2022-07-10 11:58:19 +0700

    Memperbaiki function sesuai perubahan pada jabatan (hak akses)

--- app/config/f_pegawai.php
+++ app/config/f_pegawai.php
@@ -9,15 +9,31 @@ function tambah($data)
     $email = $data["email"];
     $password = $data["password"];
     $nama = $data["nama"];
-    $jabatan = $data["jabatan"];
     $no_telp = $data["no_telp"];
     $thn_jabatan = $data["thn_jabatan"];
     $status = $data["status"];
+    $id_jabatan = $data["id_jabatan"];
+    $jml_jabatan = count($id_jabatan);
 
+    // var_dump($id_jabatan);
+    // var_dump($jml_jabatan);
 
-    $query = "INSERT INTO tb_pegawai (nip_npak, email, password, nama, jabatan, thn_jabatan, status, no_telp) VALUES ('$nip_npak', '$email', '$password','$nama','$jabatan', '$thn_jabatan', '$status','$no_telp')";
+    $query = "INSERT INTO tb_pegawai (nip_npak, email, password, nama, thn_jabatan, status, no_telp) VALUES ('$nip_npak', '$email', '$password','$nama', '$thn_jabatan', '$status','$no_telp')";
     mysqli_query($conn, $query);
-    return mysqli_affected_rows($conn);
+    // return mysqli_affected_rows($conn);
+
+    if (mysqli_affected_rows($conn) > 0) {
+        for ($i = 0; $i < $jml_jabatan; $i++) {
+            mysqli_query($conn, "INSERT INTO tb_hak_akses (nip_npak, id_jabatan) VALUES ('$nip_npak', '$id_jabatan[$i]')");
+        }
+        if (mysqli_affected_rows($conn) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
 
 
@@ -128,15 +144,49 @@ function edit($data)
     $email = $data["email"];
     $password = $data["password"];
     $nama = $data["nama"];
-    $jabatan = $data["jabatan"];
     $no_telp = $data["no_telp"];
     $thn_jabatan = $data["thn_jabatan"];
     $status = $data["status"];
+    // $id_jabatan = $data["id_jabatan"];
+    // $jml_jabatan = count($id_jabatan);
 
+    // var_dump($nip_npak);
+    // die();
 
-    $query = "UPDATE tb_pegawai SET email='$email', password='$password', nama='$nama', jabatan='$jabatan', thn_jabatan='$thn_jabatan', status='$status', no_telp='$no_telp' WHERE nip_npak='$nip_npak'";
+    // $h = mysqli_query($conn, "SELECT * FROM tb_hak_akses WHERE nip_npak='$nip_npak'");
+    // $result = mysqli_fetch_array($q);
+    // $n = 0;
+    // foreach ($h as $row) {
+    //     echo '<pre>';
+    //     var_dump($row['id_jabatan']);
+    //     echo '</pre>';
+
+    //     foreach ($id_jabatan as $id) {
+    //         echo '<pre>';
+    //         var_dump('xxxx' . $id);
+    //         echo '</pre>';
+    //     }
+
+    // $n++;
+    // }
+    // die();
+
+    $query = "UPDATE tb_pegawai SET email='$email', password='$password', nama='$nama', thn_jabatan='$thn_jabatan', status='$status', no_telp='$no_telp' WHERE nip_npak='$nip_npak'";
     mysqli_query($conn, $query);
     return mysqli_affected_rows($conn);
+    // if (mysqli_affected_rows($conn) > 0) {
+    // for ($a = 0; $a < $jml_jabatan; $a++) {
+    //     // var_dump($id_jabatan[$a]);
+    //     mysqli_query($conn, "UPDATE tb_hak_akses SET id_jabatan = '$id_jabatan[$a]' WHERE nip_npak='$nip_npak'");
+    // }
+    // if (mysqli_affected_rows($conn) > 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    // } else {
+    //     return false;
+    // }
 }
 
 
@@ -144,9 +194,19 @@ function edit($data)
 function hapus($id)
 {
     global $conn;
+    // mysqli_query($conn, "DELETE FROM tb_hak_akses WHERE nip_npak = '$id'");
+    // if (mysqli_affected_rows($conn) > 0) {
+
     mysqli_query($conn, "DELETE FROM tb_pegawai WHERE nip_npak = '$id'");
 
-    return mysqli_affected_rows($conn);
+    // if (mysqli_affected_rows($conn) > 0) {
+    return (mysqli_affected_rows($conn));
+    //     } else {
+    //         return false;
+    //     }
+    // } else {
+    //     return false;
+    // }
 }
 
 
